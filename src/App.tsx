71a1f2ccc489fd1b86306d13a003f57fb0485fa4
TNsaturday <tnsaturday@gmail.com>
2022-05-18 14:20:03 +0300

    Add NewItemForm, render sample layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { AppContainer } from "./styles";
 import { Column } from "./components/Column";
 import {Card} from "./components/Card";
+import {AddNewItem} from "./components/AddNewItem";
 
 function App() {
+    const onAdd = (text: string) => console.log(text);
+
     return (
         <AppContainer>
             <Column text="Column Title">
                 <Card text="Card text"/>
             </Column>
+            <AddNewItem onAdd={onAdd} toggleButtonText="Add another list" />
         </AppContainer>
     );
 }
