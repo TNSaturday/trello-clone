fcaf940cbc5f57acca2dccb4c83a005a613aa0b9
TNsaturday <tnsaturday@gmail.com>
2022-05-18 14:32:26 +0300

    Add useFocus hook

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { ColumnContainer, ColumnTitle } from "../styles";
+import {AddNewItem} from "./AddNewItem";
 
 type ColumnProps = {
     text: string;
@@ -11,6 +12,11 @@ export const Column: FC<ColumnProps> = ({ text, children }: ColumnProps) => {
         <ColumnContainer>
             <ColumnTitle>{text}</ColumnTitle>
             {children}
+            <AddNewItem
+                onAdd={console.log}
+                toggleButtonText="+ Add another task"
+                dark
+            />
         </ColumnContainer>
     );
 }
