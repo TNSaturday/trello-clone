2140a4626955e9fd7fcca4458e7fcd76eed833dc
TNsaturday <tnsaturday@gmail.com>
2022-05-18 18:53:57 +0300

    Implement move list action and necessary utils

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,11 +1,21 @@
 export type Action =
     {
-        type: "ADD_LIST",
-        payload: string
+        type: "ADD_LIST";
+        payload: string;
     } |
     {
-        type: "ADD_TASK",
-        payload: { text: string, listId: string }
+        type: "ADD_TASK";
+        payload: {
+            text: string;
+            listId: string
+        };
+    } |
+    {
+        type: "MOVE_LIST";
+        payload: {
+            draggedId: string;
+            hoverId: string;
+        };
     };
 
 export const addTask = (
@@ -24,3 +34,14 @@ export const addList = (
     type: "ADD_LIST",
     payload: text
 });
+
+export const moveList = (
+    draggedId: string,
+    hoverId: string,
+): Action => ({
+    type: "MOVE_LIST",
+    payload: {
+        draggedId,
+        hoverId,
+    }
+})
